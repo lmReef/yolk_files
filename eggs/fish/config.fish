3b003600c76b0793085ee6ba9db6e1023349a613
lmReef <ReefMatson@proton.me>
2026-04-24 11:20:51 +1200

    add: jj fish completions, remove brew

--- eggs/fish/config.fish
+++ eggs/fish/config.fish
@@ -8,13 +8,8 @@ if status is-interactive
     set -x EDITOR nvim
 
     # set up tools
-    if test -d /home/linuxbrew/
-        /home/linuxbrew/.linuxbrew/bin/brew shellenv fish | source
-        for i in /home/linuxbrew/.linuxbrew/share/fish/vendor_completions.d/*
-            source $i
-        end
-    end
     mise activate fish | source
     zoxide init fish | source
     fzf --fish | source
+    jj util completion fish | source
 end
