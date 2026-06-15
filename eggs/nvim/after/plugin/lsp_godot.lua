00e49f6c940d6889838cff98ab6c1383ef0dd2c0
lmReef <git@reefmatson.com>
2026-06-16 07:53:43 +1200

    tweak: nvim flutter, lsp_godot fix warn

--- eggs/nvim/after/plugin/lsp_godot.lua
+++ eggs/nvim/after/plugin/lsp_godot.lua
@@ -1,6 +1,6 @@
 vim.lsp.config["Godot"] = {
 	cmd = vim.lsp.rpc.connect("127.0.0.1", 6005),
-	filetypes = { "gdscript", "godot" },
+	filetypes = { "gdscript" },
 	root_markers = { "project.godot", ".git" },
 	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 }
