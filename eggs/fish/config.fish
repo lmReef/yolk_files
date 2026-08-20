840c2e9ffae22434ab16164079322b72219f1b41
lmReef <git@reefmatson.com>
2026-08-20 16:57:35 +1200

    tweak: remove pnpm config from fish

--- eggs/fish/config.fish
+++ eggs/fish/config.fish
@@ -15,10 +15,4 @@ if status is-interactive
     jj util completion fish | source
     wezterm shell-completion --shell fish | source
 
-    # pnpm
-    set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-    if not string match -q -- "$PNPM_HOME/bin" $PATH
-        set -gx PATH "$PNPM_HOME/bin" $PATH
-    end
-    # pnpm end
 end
